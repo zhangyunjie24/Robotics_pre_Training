a3af3ca1d6b7a0132d61e33de7e8a983696cdcf5
zhangyunjie24 <1918275469@qq.com>
2024-09-24 15:19:27 +0800

    add *.o & terminal made .exe

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "HW.h"   /*include本质就是将头文件中的函数声明代码复制到这里*/ 
+#include<stdio.h>
 
 int main () {
     int i;   
@@ -6,5 +7,6 @@ int main () {
     print_hello();
     printf("I love programming!\n");
     printf("this is featQ\n");
+    printf("This is featK\n");
     return 0;
 }
